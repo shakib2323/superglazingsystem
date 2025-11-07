75a06aac9b1d1633be87d82b99f0f6d61e3c9ae4
Shakib Ali <shakibscro@gmail.com>
2025-11-07 22:36:09 +0530

    Add files via upload

---
+++ ascii.java
@@ -0,0 +1,11 @@
+public class ascii {
+
+    public static void main(String[] args)
+    {
+        for(int i=32;i<=127;i++)
+        {
+            System.out.println("Ascii valu of "+(char)i+": "+i);
+        }
+    }
+
+}
